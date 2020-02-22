26fd63067ea682985bf815334ca260a76d336037
suju <2567861663@qq.com>
2020-02-22 19:04:02 +0800

    增加加密m3u8下载支持

---
+++ src/main/java/com/hdl/m3u8/bean/M3U8Item.java
@@ -0,0 +1,17 @@
+package com.hdl.m3u8.bean;
+
+/**
+ * Created by suju by 2020/2/11
+ */
+public interface M3U8Item extends Comparable {
+
+    String getFile();
+
+    String getFileName();
+
+    long getLongDate();
+
+    float getSeconds();
+
+    String getType();
+}
